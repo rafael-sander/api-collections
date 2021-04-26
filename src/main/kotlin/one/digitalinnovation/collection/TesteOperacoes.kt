cfeb93a4b8bc9c296dbbe280d58f1446da6d74a5
rafael-sander <rafael.sander@gmail.com>
2021-04-26 17:51:32 -0300

    created operation class part2

--- src/main/kotlin/one/digitalinnovation/collection/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteOperacoes.kt
@@ -15,5 +15,15 @@ fun main() {
 
     val salariosMaiorQue2500 = salarios.filter { it>2500.0 } //seleciona valores >2500.0
     salariosMaiorQue2500.forEach { println(it) }
+    println("----------------")
 
+    println(salarios.count { it in 2000.0..5000.0 }) //qtos elementos em um range
+    println("----------------")
+
+    println(salarios.find { it == 2250.0 }) //encontra um elemento
+    println(salarios.find { it == 500.0 })  //não encontra nada
+    println("----------------")
+
+    println(salarios.any { it == 1000.0 }) //existe o valor?
+    println(salarios.any { it == 500.0 })
 }
